869e3f4afbe2ab3c7b9e4c075c2cf19de3f32a70
KyotoAnime <30362911+KyotoAnime@users.noreply.github.com>
2018-09-27 12:52:00 +0800

    JBF

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from skimage.io import imread, imshow, imsave
 import skimage
 import cv2
 
-def rgb2gray(I):
+def rgb2gray(I, wr=0.299, wg=0.587, wb=0.114):
     
     R = I[:, :, 0]
     G = I[:, :, 1]
     B = I[:, :, 2]
     
-    I_gray = 0.299*R + 0.587*G + 0.114*B
+    I_gray = wr*R + wg*G + wb*B
     
     return I_gray
 
@@ -24,13 +24,14 @@ def GaussainKernel(k, sigma):
     
     if k % 2 == 0:
         raise Exception('k must be an odd number')
-        
-    center = (k-1) / 2
+
     K = np.indices((k, k))
-#    K = K / (k-1)
+    
+    center = (k-1) / 2
     center = np.array([[[center]], [[center]]])
     center = np.repeat(center, k, 1)
     center = np.repeat(center, k, 2)
+    
     K = K - center
     K = K**2
     K = np.sum(K, axis=0)
@@ -52,6 +53,7 @@ def RangeKernel(K, sigma):
     if len(K.shape) == 2:
         
         center = K[center, center]
+        
         K = K - center   
         K = K**2
         K = -K
@@ -63,13 +65,13 @@ def RangeKernel(K, sigma):
         center = K[center:center+1, center:center+1]
         center = np.repeat(center, k, 1)
         center = np.repeat(center, k, 0)
+        
         K = K - center
         K = K**2
         K = np.sum(K, axis=2)
         K = -K
         K = K / (2 * sigma**2)
         K = np.exp(K)
-
         
     K = np.expand_dims(K, axis=2)
     K = np.repeat(K, 3, axis=2)
@@ -78,11 +80,12 @@ def RangeKernel(K, sigma):
 
 def JBF(I, G, k, sigma_s, sigma_r):
     
-    I_filtered = np.zeros(I.shape)
     H, W, C = I.shape
+    I_filtered = np.zeros(I.shape)
+    
     r = int((k-1) / 2)
-    I = np.pad(I, [(r, r), (r, r), (0, 0)], 'edge')
-    G = np.pad(I, [(r, r), (r, r), (0, 0)], 'edge')
+    I = np.pad(I, [(r, r), (r, r), (0, 0)], 'reflect')
+    G = np.pad(G, [(r, r), (r, r), (0, 0)], 'reflect')
 
     Ks = GaussainKernel(k, sigma_s)
     
@@ -91,14 +94,12 @@ def JBF(I, G, k, sigma_s, sigma_r):
             
             h_ = h + r
             w_ = w + r
+            
             Kr = RangeKernel(G[h_-r:h_+r+1, w_-r:w_+r+1], sigma_r)
-            
-            
-            K = Ks * Kr
-            
+            K = Ks * Kr        
             sum_ = np.sum(K, axis=1)
             sum_ = np.sum(sum_, axis=0)
-            
+                   
             rgb = I[h_-r:h_+r+1, w_-r:w_+r+1] * K
             rgb = np.sum(rgb, axis=1)
             rgb = np.sum(rgb, axis=0)
@@ -110,16 +111,13 @@ def JBF(I, G, k, sigma_s, sigma_r):
 
 I = imread('I.png')
 G = imread('G.png')
+
 I = skimage.color.rgba2rgb(I)*255
 G = skimage.color.rgba2rgb(G)*255
 I_filtered = JBF(I, G, 3, 8, 0.2)
-I_filtered = I_filtered.astype('int')
+for i in range(4):
+    I_filtered = JBF(I_filtered, G, 3, 8, 0.2)
+I_filtered = I_filtered.astype('uint8')
 imsave('I_filtered.png', I_filtered)
 
-#I_filtered = cv2.ximgproc.jointBilateralFilter(G, I, 50, 0.2, 8)
-#    
-#I_filtered = I_filtered.astype('int')
-#cv2.imwrite('I_filtered.png', I_filtered)
-    
-    
     
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ Created on Wed Sep 26 17:37:10 2018
 
 @author: user
 """
-
+import cv2
 import numpy as np
 from skimage.io import imread
 def RangeKernel(K, sigma):
@@ -51,3 +51,29 @@ a = np.array([[[1, 2, 5]]])
 
 c = np.array([5.0])
 c = c.astype('int')
+
+def GaussainKernel(k, sigma):
+    
+    if k % 2 == 0:
+        raise Exception('k must be an odd number')
+        
+    center = (k-1) / 2
+    K = np.indices((k, k))
+#    K = K / (k-1)
+    center = np.array([[[center]], [[center]]])
+    center = np.repeat(center, k, 1)
+    center = np.repeat(center, k, 2)
+    K = K - center
+    K = K**2
+    K = np.sum(K, axis=0)
+    K = -K
+    K = K / (2 * sigma**2)
+    K = np.exp(K)
+    
+    print(K/np.sum(K))
+    
+    K = np.expand_dims(K, axis=2)
+    K = np.repeat(K, 3, axis=2)
+    
+    return K
+GaussainKernel(5, 1)
