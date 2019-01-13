d83f2dc66aa73545ebb006a3dff2c08272fbf21b
Kyotoanime <b04505004@ntu.edu.tw>
2019-01-13 19:22:14 +0800

    Update dispnet.py

--- Final_Project/dispnet.py
+++ Final_Project/dispnet.py
@@ -76,6 +76,8 @@ def train(**kwargs):
             soft_label[0, 0:4] = t.tensor([0.5, 0.3, 0.15, 0.05])
             soft_label = soft_label.repeat(d.shape[0], 1)
             output = model(left, right, train=True)
+            print(output.shape)
+            print(soft_label.shape)
             loss = -(output*soft_label).mean()
 
             optimizer.zero_grad()
