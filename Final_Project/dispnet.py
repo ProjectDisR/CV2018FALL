ae7f9446fc4ef34ad2fa48020dcd00feae091f6b
Kyotoanime <b04505004@ntu.edu.tw>
2019-01-14 21:02:29 +0800

    Update dispnet.py

--- Final_Project/dispnet.py
+++ Final_Project/dispnet.py
@@ -29,7 +29,6 @@ import fire
 def hisEqulColor(img):
 	ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
 	channels = cv2.split(ycrcb)
-	print(len(channels))
 	cv2.equalizeHist(channels[0], channels[0])
 	cv2.merge(channels, ycrcb)
 	cv2.cvtColor(ycrcb, cv2.COLOR_YCR_CB2BGR, img)
