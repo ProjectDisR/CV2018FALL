e9505e86f7e846f42fe08fd3ca381056e4c0ed5d
Kyotoanime <b04505004@ntu.edu.tw>
2019-01-15 16:42:35 +0800

    Update main.py

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -6,6 +6,7 @@ import torch
 import torchvision as tv
 
 from models.model import Model
+from optimize import Optimize
 
 from util import writePFM
 
@@ -14,6 +15,15 @@ parser.add_argument('--input-left', default='./data/Synthetic/TL0.png', type=str
 parser.add_argument('--input-right', default='./data/Synthetic/TR0.png', type=str, help='input right image')
 parser.add_argument('--output', default='./TL0.pfm', type=str, help='left disparity map')
 
+def hisEqulColor(img):
+    
+	ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+	channels = cv2.split(ycrcb)
+	cv2.equalizeHist(channels[0], channels[0])
+	cv2.merge(channels, ycrcb)
+	cv2.cvtColor(ycrcb, cv2.COLOR_YCR_CB2BGR, img)
+    
+	return img
 
 # You can modify the function interface as you like
 def computeDisp(Il, Ir):
@@ -47,6 +57,25 @@ def computeDisp(Il, Ir):
     
     return disp
 
+def Disparity(img_left, img_right):
+    img_left = hisEqulColor(img_left)
+    img_right = hisEqulColor(img_right)
+    # Mirror image
+    img_left_1 = np.fliplr(img_right)
+    img_right_1 = np.fliplr(img_left)
+    
+    disp = computeDisp(img_left, img_right)              
+    disp_1 = computeDisp(img_left_1, img_right_1)
+    disp_1 = np.fliplr(disp_1)
+    
+    disp = np.int32(disp)
+    disp_1 = np.int32(disp_1)
+
+    disp = Optimize(img_left,disp,disp_1)
+    disp = cv2.medianBlur(np.uint8(disp), 11)
+    disp = disp.astype(np.float32)
+    
+    return disp
 
 def main():
     args = parser.parse_args()
@@ -55,7 +84,7 @@ def main():
     img_left = cv2.imread(args.input_left)
     img_right = cv2.imread(args.input_right)
     tic = time.time()
-    disp = computeDisp(img_left, img_right)
+    disp = Disparity(img_left, img_right)
     toc = time.time()
     writePFM(args.output, disp)
     print('Elapsed time: %f sec.' % (toc - tic))
